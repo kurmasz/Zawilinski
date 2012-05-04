d48785d6a2f385e91204d38e51fd5eb305cb2b15
Zachary Kurmas <kurmasz@gvsu.edu>
2012-05-03 22:54:53 -0400

    Changed Main-Class for jar file to print build info.

--- src/edu/gvsu/kurmasz/zawilinski/Zawilinski.java
+++ src/edu/gvsu/kurmasz/zawilinski/Zawilinski.java
@@ -1,48 +1,50 @@
 package edu.gvsu.kurmasz.zawilinski;
 
+import edu.gvsu.kurmasz.warszawa.util.BuildInfo;
+
 /**
- * Created by IntelliJ IDEA.
- * User: kurmasz
- * Date: 4/19/12
- * Time: 4:48 PM
- * To change this template use File | Settings | File Templates.
+ * Constants shared by entire Zawilinski library
  */
 public class Zawilinski {
 
-    //////////////////////////////////////////////////
-    //
-    // Logging levels
-    //
-    /////////////////////////////////////////////////
+   //////////////////////////////////////////////////
+   //
+   // Logging levels
+   //
+   /////////////////////////////////////////////////
 
-    /**
-     * Debug level for the beginning and end of parsing.
-     */
-    public static final int PARSE_BEGIN_END = 500;
+   /**
+    * Debug level for the beginning and end of parsing.
+    */
+   public static final int PARSE_BEGIN_END = 500;
 
-    /**
-     * Debug level used to list entries truncated by text size
-     */
-    public static final int TRUNCATIONS = PARSE_BEGIN_END - 10;
+   /**
+    * Debug level used to list entries truncated by text size
+    */
+   public static final int TRUNCATIONS = PARSE_BEGIN_END - 10;
 
-    /**
-     * Debug level used to list pages dumped by post-filter
-     */
-    public static final int PAGE_DUMPED = TRUNCATIONS - 10;
+   /**
+    * Debug level used to list pages dumped by post-filter
+    */
+   public static final int PAGE_DUMPED = TRUNCATIONS - 10;
 
-    /**
-     * Debug level used to list revisions dumped by post-filter
-     */
-    public static final int REVISION_DUMPED = PAGE_DUMPED - 1;
-    /**
-     * Debug level used to list pages kept by post-filter
-     */
-    public static final int PAGE_KEPT = REVISION_DUMPED - 10;
+   /**
+    * Debug level used to list revisions dumped by post-filter
+    */
+   public static final int REVISION_DUMPED = PAGE_DUMPED - 1;
+   /**
+    * Debug level used to list pages kept by post-filter
+    */
+   public static final int PAGE_KEPT = REVISION_DUMPED - 10;
 
-    /**
-     * Debug level used to list revisions kept by post-filter
-     */
-    public static final int REVISION_KEPT = PAGE_KEPT - 1;
+   /**
+    * Debug level used to list revisions kept by post-filter
+    */
+   public static final int REVISION_KEPT = PAGE_KEPT - 1;
 
-
+   public static void main(String[] args) {
+      BuildInfo bi = BuildInfo.make(Zawilinski.class);
+      System.out.println("Zawilinski version " + bi.getVersion() + " built "
+            + bi.getBuildDate() + ".");
+   }
 }
