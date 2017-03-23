e3176fe67d92f1b15b25ae260238efc532c82426
Zachary Kurmas <kurmasz@users.noreply.github.com>
2017-03-23 16:45:31 -0400

    Prepared regression data file containing expected output of sorting selected words for Polish

--- system_tests/scripts/count_language_revisions.rb
+++ system_tests/scripts/count_language_revisions.rb
@@ -3,11 +3,25 @@
 # number of revisions for each page containing the target langauge.
 #
 
-language = ARGV[0];
+
+# Count and display info for only those pages containing 
+# data for the target language
+language_only = ARGV.any? {|item| item == "--languageOnly"}
+
+# Show the total number of revisions --- including those that do not 
+# include dtata for the target langauge
+show_total_revisions = ARGV.any? {|item| item == "--showTotalRevs"}
+
+# Remove any options from the command-line parameters.
+# NOTE:  This only works if all possible parameters take 
+# no options!
+non_options = ARGV.reject {|item| item.start_with?("--")}
+
+language = non_options[0]
 
 title = nil
-revision_count = 0
-language_count = 0
+total_revisions = 0           # Total number of revisions for this page
+revisions_with_language = 0   # Number of revisions containing the target language
 
 in_page = false
 
@@ -20,10 +34,13 @@ while line = STDIN.gets
     title = "<no title yet>"
   elsif (line =~ /<\/page>/)
     in_page = false
-    puts "%10d %4d %4d %s" % [page_count, language_count, revision_count, title]
-    page_count += 1
-    revision_count = 0
-    language_count = 0
+    if (!language_only || revisions_with_language != 0) 
+      total_revisions_format = show_total_revisions ? "%4d " : ""  # only show total revisions when requested.
+      puts "%10d %4d #{total_revisions_format}#{title}" % [page_count, revisions_with_language, total_revisions]
+      page_count += 1
+    end
+    total_revisions = 0
+    revisions_with_language = 0
   elsif (line =~ /<title>(.+)<\/title>/)
     title = $1
     title.gsub!('&quot;', '"')
@@ -31,7 +48,7 @@ while line = STDIN.gets
     if (!in_page)
       puts("ERROR! Found title outside page. #{title}")
     end
-    revision_count = 0
+    total_revisions = 0
   elsif (line =~ /<revision>/)
     in_revision = true
     foundOnce = false
@@ -39,7 +56,7 @@ while line = STDIN.gets
       puts("ERROR! Found revision outside page. #{title}")
     end
 
-    revision_count += 1
+    total_revisions += 1
 
     # The full regular expression for a language header is expensive (time-wise)
     # so, we first do a quick check for the bare language before
@@ -50,7 +67,7 @@ while line = STDIN.gets
     # are usually condensed into one within a few revisions.
     # (2) the <comment> section occasionally contains ==Polish==
     if (!foundOnce)
-      language_count += 1
+      revisions_with_language += 1
     end
     foundOnce = true
     if (!in_revision)
